f7ff01e7cac22df20bd30b79f2a30ce08c20ab5e
Devzando <74877494+Devzando@users.noreply.github.com>
2023-03-10 10:49:22 -0300

    add jogo de batalha naval

---
+++ JogoBatalhaNaval/src/SimpleStartupGame.java
@@ -0,0 +1,32 @@
+class SimpleStartupGame {
+    
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        
+        GameHelper helper = new GameHelper();
+
+        SimpleStartup theStartup = new SimpleStartup();
+
+        int randonNum = (int) (Math.random() * 5);
+
+        int[] locations = {randonNum, randonNum +1, randonNum +2};
+
+        theStartup.setLocationCells(locations);
+
+        boolean isAlive = true;
+
+        while(isAlive){
+            int guess = helper.getUserInput("enter a number");
+            String result = theStartup.checkYourself(guess);
+
+            numOfGuesses++;
+
+            if(result.equals("kill")){
+                isAlive = false;
+
+                System.out.println("You took " + numOfGuesses + "guesses");
+            }
+        }
+    }
+    
+}
